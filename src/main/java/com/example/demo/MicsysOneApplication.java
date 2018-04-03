0dd0c87e9ed821690cf44c247f23918676a8d163
fengzikai111 <fengzikai111@163.com>
2018-04-03 15:34:50 +0800

    init springcloud demo one

--- src/main/java/com/example/demo/MicsysOneApplication.java
+++ src/main/java/com/example/demo/MicsysOneApplication.java
@@ -8,6 +8,7 @@ public class MicsysOneApplication {
 
 	public static void main(String[] args) {
 		System.out.println("this is first demo about micsys");
+		System.out.println("===========");
 		SpringApplication.run(MicsysOneApplication.class, args);
 	}
 }
